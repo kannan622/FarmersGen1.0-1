83354b356b7d855415aed6ebe0c7ddfea0ca040a
Saravana Murali <saravanamurali24@gmail.com>
2019-05-05 13:04:28 +0530

    Getting Lat and Lanti Commit

--- app/src/main/java/com/example/saravanamurali/farmersgen/address/UpdateAddress_Activity.java
+++ app/src/main/java/com/example/saravanamurali/farmersgen/address/UpdateAddress_Activity.java
@@ -1,18 +1,37 @@
 package com.example.saravanamurali.farmersgen.address;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.saravanamurali.farmersgen.MainActivity;
 import com.example.saravanamurali.farmersgen.R;
 import com.example.saravanamurali.farmersgen.apiInterfaces.ApiInterface;
+import com.example.saravanamurali.farmersgen.models.GetGeoAddressDTO;
 import com.example.saravanamurali.farmersgen.paymentgateway.PaymentGatewayActivity;
 import com.example.saravanamurali.farmersgen.models.UpdateAddressDTO;
 import com.example.saravanamurali.farmersgen.retrofitclient.APIClientToUpdateAddress;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -20,11 +39,14 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class UpdateAddress_Activity extends AppCompatActivity {
+public class UpdateAddress_Activity extends AppCompatActivity implements View.OnClickListener {
 
     private  String NO_CURRENT_USER="NO_CURRENT_USER";
 
     String addressID;
+    Geocoder geocoder;
+    List<Address> getGeoAddresses;
+    double latti,longi;
 
     private TextInputLayout uFlatNo;
     private TextInputLayout uStreetName;
@@ -45,6 +67,11 @@ public class UpdateAddress_Activity extends AppCompatActivity {
     private String proceed_LandMark="";
     private String proceed_Alternate_Mobile_Number="";
 
+    private static final int REQUEST_LOCATION = 1;
+    private Button myLocationButton;
+    LocationManager locationManager;
+    String lattitude,longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +90,15 @@ public class UpdateAddress_Activity extends AppCompatActivity {
 
         uLandMark=findViewById(R.id.updateLandMark);
         uAlternateMobile=findViewById(R.id.updateAlternateMobile);
+
+        myLocationButton=(Button)findViewById(R.id.myLocationButton);
+
+       myLocationButton.setOnClickListener(this);
+
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+
+
 
 
     }
@@ -104,11 +140,6 @@ public class UpdateAddress_Activity extends AppCompatActivity {
         return status;
 
     }
-
-
-
-
-
 
 
     private boolean validateFlatNo(){
@@ -277,7 +308,105 @@ public class UpdateAddress_Activity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+
+        }
+        else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            getLocation();
+            
+            convertingLatandLanIntoAddress();
+        }
+
+    }
+
+    private void convertingLatandLanIntoAddress() {
+        geocoder=new Geocoder(this, Locale.getDefault());
+
+        try {
+            getGeoAddresses=geocoder.getFromLocation(latti,longi,1);
+         String address=   getGeoAddresses.get(0).getAddressLine(0);
+            String area =   getGeoAddresses.get(0).getLocality();
+            String city=  getGeoAddresses.get(0).getAdminArea();
+           String country= getGeoAddresses.get(0).getCountryName();
+            String postal =getGeoAddresses.get(0).getPostalCode();
+
+            Toast.makeText(UpdateAddress_Activity.this,address+" "+area+" "+city+" "+country+" "+postal,Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(UpdateAddress_Activity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (UpdateAddress_Activity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(UpdateAddress_Activity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        }
+
+        else {
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            Location location2 = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (location != null) {
+                latti = location.getLatitude();
+                 longi = location.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+                Toast.makeText(UpdateAddress_Activity.this, "First" + lattitude + " " + longitude, Toast.LENGTH_LONG).show();
 
 
+            } else if (location1 != null) {
+                latti = location1.getLatitude();
+                longi = location1.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+                Toast.makeText(UpdateAddress_Activity.this, "Second" + lattitude + " " + longitude, Toast.LENGTH_LONG).show();
+
+            } else if (location2 != null) {
+                latti = location2.getLatitude();
+                longi = location2.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+                Toast.makeText(UpdateAddress_Activity.this, "Third" + lattitude + " " + longitude, Toast.LENGTH_LONG).show();
+            }
+
+            else{
+
+                Toast.makeText(this,"Unble to Trace your location",Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Please Turn ON your GPS Connection")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+         AlertDialog alert = builder.create();
+        alert.show();
+    }
 
 }
