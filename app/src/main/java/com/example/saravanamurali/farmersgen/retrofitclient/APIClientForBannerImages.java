d175e89e3b1d8fdfca9ae3c5a3d664ff80f32bbd
saravanamurali <saravanamurali24@gmail.com>
2019-06-27 16:29:10 +0530

    Banner Images Commit

--- app/src/main/java/com/example/saravanamurali/farmersgen/retrofitclient/APIClientForBannerImages.java
+++ app/src/main/java/com/example/saravanamurali/farmersgen/retrofitclient/APIClientForBannerImages.java
@@ -2,19 +2,38 @@ package com.example.saravanamurali.farmersgen.retrofitclient;
 
 import com.example.saravanamurali.farmersgen.apiInterfaces.ApiInterface;
 import com.example.saravanamurali.farmersgen.baseurl.BaseUrl;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClientForBannerImages implements BaseUrl {
 
     public static Retrofit retrofit=null;
+    private static Gson gson = new GsonBuilder()
+            .setLenient()
+            .create();
 
     public static Retrofit getRetrofitClientForBannerImages(){
 
         if(retrofit==null) {
 
-            retrofit = new Retrofit.Builder().baseUrl(BaseUrl.ROOT_URL_FOR_BANNER_IMAGES).addConverterFactory(GsonConverterFactory.create()).build();
+            OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                    .retryOnConnectionFailure(true)
+                    .connectTimeout(60, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .writeTimeout(60, TimeUnit.SECONDS)
+                    .build();
+
+
+            retrofit = new Retrofit.Builder().baseUrl(BaseUrl.ROOT_URL_FOR_BANNER_IMAGES)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .client(okHttpClient)
+                    .build();
 
         }
         return retrofit;
